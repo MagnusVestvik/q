362037fe02501e04739ad15021db7d8176c9712b
MagnusVestvik <magnusvestvik@proton.me>
2025-04-12 23:17:49 +0200

    FEATURE: add symbols for making it easier to print
    NB this is bugged need to be string for adding newlines etc when using
    some of them but gotta find the rust way to do this.

--- src/display/printer.rs
+++ src/display/printer.rs
@@ -1,4 +1,6 @@
-use crate::color::Color;
+use super::color::Color;
+use super::symbols::{self, Symbols};
+use string_builder::Builder;
 
 pub struct Printer {
     folder_color: Color,
@@ -7,18 +9,21 @@ pub struct Printer {
     show_size: bool,
 }
 
-impl Printer {
+// We should have name, type, size and modified. maybe skip the option of having size and make it
+// default
+
+impl Printer { // TODO: missing logic for actually drawing header text
     fn new(
-        folder_color: String,
-        files_color: String,
-        border_color: String,
+        folder_color: Color,
+        files_color: Color,
+        border_color: Color,
         show_size: bool,
     ) -> Printer {
         Printer {
-            folder_color: folder_color,
-            files_color: files_color,
-            border_color: border_color,
-            show_size: show_size,
+            folder_color,
+            files_color,
+            border_color,
+            show_size,
         }
     }
 
@@ -32,5 +37,35 @@ impl Printer {
 
     fn draw_input_vertically(input: Vec<String>) -> String {
         input.join("\n")
+    }
+
+    fn draw_input_show_size() -> String {
+        Symbols::HORIZONTAL.to_string()
+    }
+
+    fn draw_header(&self, width: usize, height: usize) -> String {
+        let mut sb = Builder::default(); // TODO: missing logic for actually drawing header text
+        sb.append("-".repeat(width));
+        sb.append("|\n".repeat(height))
+
+        sb.string().unwrap()
+    }
+
+    fn draw_body(&self, input: Vec<String>) -> String{
+        "".to_string()
+    }
+    fn draw_body_with_size(&self, input: Vec<String>, size: Vec<String>) -> String{
+        "".to_string()
+    }
+
+    fn draw_footer(&self) -> String{
+        "".to_string()
+    }
+
+    fn draw_box() -> String{
+        let mut sb = Builder::default();
+        sb.append(Symbols::HORIZONTAL.to_string().repeat(4));
+        sb.append(Symbols::VERTICAL.to_string().repeat(4) + &" ".to_string().repeat(4) + &Symbols::HORIZONTAL.to_string().repeat(4));
+        "".to_string()
     }
 }
---
+++ src/display/symbols.rs
@@ -0,0 +1,13 @@
+pub mod Symbols {
+    pub const HORIZONTAL: String = "─";
+    pub const VERTICAL: String = "│";
+    pub const TOP_LEFT: String = "┌";
+    pub const BOTTOM_LEFT: String = "└";
+    pub const TOP_RIGHT: String = "┐";
+    pub const BOTTOM_RIGHT: String = "┘";
+    pub const T_DOWN: String = "┬";
+    pub const T_UP: String = "┴";
+    pub const T_LEFT: String = "├";
+    pub const T_RIGHT: String = "┤";
+    pub const CROSS: String = "┼";
+}
