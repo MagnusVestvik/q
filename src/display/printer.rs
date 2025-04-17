76280aa5450fe55bdeefe8c6e03bfa58dc0e5b5b
MagnusVestvik <magnusvestvik@proton.me>
2025-04-18 00:33:41 +0200

    CHANGE: refactor printer

--- src/display/printer.rs
+++ src/display/printer.rs
@@ -1,5 +1,5 @@
 use super::color::Color;
-use super::symbols;
+use super::symbols::symbols as sym;
 use string_builder::Builder;
 
 pub struct Printer {
@@ -11,9 +11,21 @@ pub struct Printer {
 
 // We should have name, type, size and modified. maybe skip the option of having size and make it
 // default
+//
+
+/*
+ * TODO: change string_builder in favour of write! macro since this is in std.
+ * it can be used like the following:
+ * write!(
+ * &mut out,
+ * "{}{}",
+ * "smth1",
+ * "smth2"
+ * )
+ * .unwrap();
+ */
 
 impl Printer {
-    // TODO: missing logic for actually drawing header text
     fn new(
         folder_color: Color,
         files_color: Color,
@@ -28,95 +40,61 @@ impl Printer {
         }
     }
 
-    // TODO: rename
-    fn createDrawItems(&self, path: String) -> Vec<Vec<String>> {
-        let names: Vec<String> = get_files_and_folders(path);
-        let types: Vec<String> = get_file_types(&names);
-        let sizes: Vec<String> = get_file_sizes(&names);
-
-        names
-            .iter()
-            .zip(types.iter())
-            .zip(sizes.iter())
-            .map(|((name, ty), size)| vec![name.clone(), ty.clone(), size.clone()])
-            .collect()
-    }
-
     fn draw_input_vertically(input: Vec<String>) -> String {
         input.join("\n")
     }
 
     fn draw_input_show_size() -> String {
-        symbols::symbols::HORIZONTAL.to_string()
+        sym::HORIZONTAL.to_string()
     }
 
     fn draw_header(&self, width: usize, height: usize) -> String {
         let mut sb = Builder::default();
         sb.append(format!(
             "{}{}{}\n",
-            symbols::symbols::TOP_LEFT,
-            symbols::symbols::HORIZONTAL.repeat(width - 2),
-            symbols::symbols::TOP_RIGHT,
+            sym::TOP_LEFT,
+            sym::HORIZONTAL.repeat(width - 2),
+            sym::TOP_RIGHT,
         ));
         sb.append(format!(
-            // TODO: should create boxes for each column here
+            // TODO: should create boxes for each column here this draw method is also wrong
             "{}{}{}{}{}{}{}",
-            symbols::symbols::VERTICAL,
+            sym::VERTICAL,
             "#",
             "Name",
             "Type",
             "Size",
             "modified",
-            symbols::symbols::VERTICAL,
+            sym::VERTICAL,
         ));
 
         sb.string().unwrap()
     }
 
-    // TODO: not extensive but works for now, maybe should take in a
-    // draw function that would be responsible for what to draw on each line or somthing similar
-    fn draw_body(&self, input: Vec<Vec<String>>) -> String {
+    fn draw_bottom(width: usize) -> String {
         let mut sb = Builder::default();
-        for i in 0..input.len() {
-            sb.append(format!(
-                "{}{}{}{}{}{}{}{}{}{}{}",
-                symbols::symbols::VERTICAL,
-                i,
-                symbols::symbols::VERTICAL,
-                input[i][0],
-                symbols::symbols::VERTICAL,
-                input[i][1],
-                symbols::symbols::VERTICAL,
-                input[i][2],
-                symbols::symbols::VERTICAL,
-                input[i][4],
-                symbols::symbols::VERTICAL,
-            ));
-            if i + 1 == input.len() {
-                sb.append(format!(
-                    "{}{}{}",
-                    symbols::symbols::BOTTOM_LEFT,
-                    symbols::symbols::HORIZONTAL.repeat(input.len() - 2),
-                    symbols::symbols::BOTTOM_RIGHT,
-                ));
-            }
-        }
+        sb.append(format!(
+            "{}{}{}",
+            sym::BOTTOM_LEFT,
+            sym::HORIZONTAL.repeat(width.saturating_sub(2)),
+            sym::BOTTOM_RIGHT,
+        ));
+
         sb.string().unwrap()
     }
-}
 
-fn get_files_and_folders(path: _) -> _ {
-    todo!()
-}
+    fn draw_body<F>(&self, input: Vec<Vec<String>>, mut draw_row: F) -> String
+    where
+        F: FnMut(Vec<String>, usize) -> String,
+    {
+        let mut to_draw: Vec<String> = input
+            .iter()
+            .enumerate()
+            .map(|(i, row)| draw_row(row.to_vec(), i))
+            .collect();
 
-fn get_file_sizes(file_and_folder_names: _) -> _ {
-    todo!()
-}
+        to_draw.push(Self::draw_bottom(to_draw.len()));
 
-fn get_file_types(file_and_folder_names: _) -> _ {
-    todo!()
-}
-
-fn get_files_and_folders_names(path: _) -> _ {
-    todo!()
+        to_draw.join("\n")
+    }
 }
