c6e2dbc0a90915c75ed37376db10d2cb6309834b
MagnusVestvik <magnusvestvik@proton.me>
2025-04-18 00:48:45 +0200

    FEATURE: add logic for getting vec of vec with what should be printed

--- src/logic/files_logic.rs
+++ src/logic/files_logic.rs
@@ -5,13 +5,37 @@ fn read_files_in_dir(path: String) -> ReadDir {
     files
 }
 
-pub fn get_files_as_list(path: String) -> Vec<String> {
+pub fn get_paths(path: String) -> Vec<String> {
     let dir_files = read_files_in_dir(path);
     dir_files
         .filter_map(|entry| {
             entry
                 .ok()
                 .and_then(|e| e.file_name().to_str().map(String::from))
+        })
+        .collect()
+}
+
+pub fn detect_path_type(path: String) -> String {
+    match path.strip_suffix("/") {
+        Some(folder) => "folder".to_string(),
+        None => "file".to_string(),
+    }
+}
+
+pub fn get_path_size(path: String) -> u64 {
+    let metadata = fs::metadata(path).unwrap();
+    metadata.len()
+}
+
+pub fn collect_path_outputs(path: String) -> Vec<Vec<String>> {
+    get_paths(path)
+        .iter()
+        .map(|path_name| {
+            let path_type = detect_path_type(path_name.clone());
+            let path_size = get_path_size(path_name.clone());
+
+            vec![path_name.clone(), path_type, path_size.to_string()]
         })
         .collect()
 }
