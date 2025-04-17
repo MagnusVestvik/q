d0af10c5260e8fd4cb63c5c6b5e3b0abe0aab1ad
MagnusVestvik <magnusvestvik@proton.me>
2025-04-17 22:23:03 +0200

    FEATURE: added method for getting all necessary items for drawing + created body draw
    implementations right now is a bit stupid and should be fixed

--- src/display/printer.rs
+++ src/display/printer.rs
@@ -28,12 +28,18 @@ impl Printer {
         }
     }
 
-    fn draw(&self, path: String, get_files_and_folders: impl Fn(String) -> Vec<String>) -> String {
-        let to_render: Vec<String> = get_files_and_folders(path);
-        if self.show_size {
-            // TODO: implement
-        }
-        "".to_string()
+    // TODO: rename
+    fn createDrawItems(&self, path: String) -> Vec<Vec<String>> {
+        let names: Vec<String> = get_files_and_folders(path);
+        let types: Vec<String> = get_file_types(&names);
+        let sizes: Vec<String> = get_file_sizes(&names);
+
+        names
+            .iter()
+            .zip(types.iter())
+            .zip(sizes.iter())
+            .map(|((name, ty), size)| vec![name.clone(), ty.clone(), size.clone()])
+            .collect()
     }
 
     fn draw_input_vertically(input: Vec<String>) -> String {
@@ -67,19 +73,50 @@ impl Printer {
         sb.string().unwrap()
     }
 
-    fn draw_body(&self, input: Vec<String>) -> String {
-        "".to_string()
-    }
-    fn draw_body_with_size(&self, input: Vec<String>, size: Vec<String>) -> String {
-        "".to_string()
-    }
-
-    fn draw_footer(&self) -> String {
-        "".to_string()
-    }
-
-    fn draw_box() -> String {
+    // TODO: not extensive but works for now, maybe should take in a
+    // draw function that would be responsible for what to draw on each line or somthing similar
+    fn draw_body(&self, input: Vec<Vec<String>>) -> String {
         let mut sb = Builder::default();
-        "".to_string()
+        for i in 0..input.len() {
+            sb.append(format!(
+                "{}{}{}{}{}{}{}{}{}{}{}",
+                symbols::symbols::VERTICAL,
+                i,
+                symbols::symbols::VERTICAL,
+                input[i][0],
+                symbols::symbols::VERTICAL,
+                input[i][1],
+                symbols::symbols::VERTICAL,
+                input[i][2],
+                symbols::symbols::VERTICAL,
+                input[i][4],
+                symbols::symbols::VERTICAL,
+            ));
+            if i + 1 == input.len() {
+                sb.append(format!(
+                    "{}{}{}",
+                    symbols::symbols::BOTTOM_LEFT,
+                    symbols::symbols::HORIZONTAL.repeat(input.len() - 2),
+                    symbols::symbols::BOTTOM_RIGHT,
+                ));
+            }
+        }
+        sb.string().unwrap()
     }
+}
+
+fn get_files_and_folders(path: _) -> _ {
+    todo!()
+}
+
+fn get_file_sizes(file_and_folder_names: _) -> _ {
+    todo!()
+}
+
+fn get_file_types(file_and_folder_names: _) -> _ {
+    todo!()
+}
+
+fn get_files_and_folders_names(path: _) -> _ {
+    todo!()
 }
